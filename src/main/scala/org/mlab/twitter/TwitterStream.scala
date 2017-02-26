6f460822fce9f01d33684ec424bd1e715eab17bf
Dawvid <dssd1001@gmail.com>
2017-02-26 13:32:39 -0800

    implement TwitterStream

--- src/main/scala/org/mlab/twitter/TwitterStream.scala
+++ src/main/scala/org/mlab/twitter/TwitterStream.scala
@@ -1,8 +1,34 @@
 package org.mlab.twitter
 
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.DStream
+import twitter4j.Status
+import org.apache.spark.streaming.twitter.TwitterUtils
+
+import scala.collection.JavaConversions._
+
 /**
   * Created by ravi on 2/22/17.
   */
 class TwitterStream {
 
+  def getDStream(): DStream[Status] = {
+    val sc = new SparkContext(new SparkConf().setAppName("stream-comp-twitter-stream-sentiment"))
+
+    sc.setLogLevel("WARN")
+
+    val ssc = new StreamingContext(sc, Seconds(5))
+
+    println("Initializing Twitter stream...")
+
+    System.setProperty("twitter4j.oauth.consumerKey", "yy80mXCK2VV12LxxEVhQfrsTx")
+    System.setProperty("twitter4j.oauth.consumerSecret", "QS518cO564jeeq5zFEg0r3UWKMOjlq4J61kC0Ny4jMN8GKh0MA")
+    System.setProperty("twitter4j.oauth.accessToken", "3317348143-DkJvR5VD3DTuXb4n8MkVJT9MPBLpjRqpd5Rm3tw")
+    System.setProperty("twitter4j.oauth.accessTokenSecret", "mJdZVyQxuublUL2y7Rwj7tNBIRJLoH5GoSavbjFI9bke0")
+
+    val stream = TwitterUtils.createStream(ssc, None)
+
+    return stream
+  }
 }
